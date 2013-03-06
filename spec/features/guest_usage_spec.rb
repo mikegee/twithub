0dd1430975327360d1573c600d89fba7ff0e14ef
mikegee <gee.24@osu.edu>
2013-03-05 21:55:54 -0500

    kill big ol H1 Timeline

--- spec/features/guest_usage_spec.rb
+++ spec/features/guest_usage_spec.rb
@@ -7,7 +7,7 @@ feature 'Guest Usage' do
   end
 
   scenario 'the user should not see a timeline yet' do
-    expect(page).to_not have_content('Timeline')
+    expect(page).to_not have_css('#timeline')
   end
 
   scenario 'the user should be able to click a login link' do
