1e7cf7a6aacf5dcee3fc89ce7afe156db81360f7
mikegee <gee.24@osu.edu>
2013-03-05 19:14:12 -0500

    showing all statuses to guests seems weird now

--- spec/features/guest_usage_spec.rb
+++ spec/features/guest_usage_spec.rb
@@ -2,30 +2,16 @@ require 'spec_helper'
 
 feature 'Guest Usage' do
 
-  context 'without any statuses' do
-    background do
-      visit '/'
-    end
-
-    scenario 'the user should be able to click a login link' do
-      click_link 'Login'
-    end
-
-    scenario 'the user should see the public timeline' do
-      expect(page).to have_content('Timeline')
-    end
+  background do
+    visit '/'
   end
 
-  context 'a single status exists' do
-    let(:content) { 'check out this cool new Twitter clone' }
-
-    background do
-      Status.stub(:all) { [mock_model(Status, content: content)] }
-      visit '/'
-    end
-
-    scenario 'the guest should see that one status in the timeline' do
-      expect(page).to have_content(content)
-    end
+  scenario 'the user should not see a timeline yet' do
+    expect(page).to_not have_content('Timeline')
   end
+
+  scenario 'the user should be able to click a login link' do
+    click_link 'Login'
+  end
+
 end
