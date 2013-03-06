f53c0ea8b991676168a26125ec6669ec48731206
mikegee <gee.24@osu.edu>
2013-03-06 10:35:21 -0500

    FavoriteMark validations

--- app/models/favorite_mark.rb
+++ app/models/favorite_mark.rb
@@ -1,5 +1,10 @@
 class FavoriteMark < ActiveRecord::Base
+
   belongs_to :user
   belongs_to :status
-  # attr_accessible :title, :body
+
+  validates_presence_of :user_id, :status_id
+
+  validates_uniqueness_of :status_id, scope: :user_id
+
 end
--- spec/models/favorite_mark_spec.rb
+++ spec/models/favorite_mark_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe FavoriteMark do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:status_id) }
+
+  it { should validate_uniqueness_of(:status_id).scoped_to(:user_id) }
+
 end
