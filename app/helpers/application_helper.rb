a7b6e8637dcdff655be2538e3d927a12d0caa68b
mikegee <gee.24@osu.edu>
2013-03-07 05:36:30 -0500

    extracted follow_button helper, put it on profiles
    [ci skip]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
     "/auth/#{provider}"
   end
 
+  def follow_button(user)
+    if current_user.users_followed.include? user
+      link_to 'Following', user_follow_path(current_user, user.id), method: 'delete', class: 'btn btn-primary following'
+    elsif user != current_user
+      link_to 'Follow', user_follows_path(current_user, follow: {followee_id: user.id}), method: 'post', class: 'btn followable'
+    end
+  end
 end
