ba85417e61bfc361fc3a92e3fcd48d43d9382600
mikegee <gee.24@osu.edu>
2013-03-06 21:43:21 -0500

    use kaminari's total_count to avoid an db query

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,7 +3,7 @@ class FollowersController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @users = @user.followers.page(params[:page])
-    unless @users.any?
+    if @users.total_count.zero?
       name = @user == current_user ? 'you' : @user.name
       flash.now[:notice] ||= "Nobody is following #{name} yet."
     end
--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,7 +3,7 @@ class FollowingsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @users = @user.users_followed.page(params[:page])
-    unless @users.any?
+    if @users.total_count.zero?
       start_of_notice = current_user == @user ? "You aren't" : "#{@user.name} isn't"
       flash.now[:notice] ||= "#{start_of_notice} following any users yet."
     end
